dc4a2763a52795107bfe3a4a716de33a96419553
Juan Calisaya <u202121935@upc.edu.pe>
2024-10-31 00:05:53 -0500

    feat(shared): add infrastructure layer for shared bounded context.

--- src/main/java/upc/edu/ecomovil/api/EcomovilApiApplication.java
+++ src/main/java/upc/edu/ecomovil/api/EcomovilApiApplication.java
@@ -2,7 +2,9 @@ package upc.edu.ecomovil.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class EcomovilApiApplication {
 
