841182a40d0fb21fd69def0ff743b6f622a5c29b
Juan Calisaya <u202121935@upc.edu.pe>
2024-10-30 23:40:55 -0500

    chore: initial commit

---
+++ src/main/java/upc/edu/ecomovil/api/EcomovilApiApplication.java
@@ -0,0 +1,13 @@
+package upc.edu.ecomovil.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcomovilApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcomovilApiApplication.class, args);
+    }
+
+}
