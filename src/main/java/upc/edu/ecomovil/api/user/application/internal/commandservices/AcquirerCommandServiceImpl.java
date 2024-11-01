6d9042d59164c77681e842e510dfb06df84b713b
mathiasadriano <129204439+mathiasadriano@users.noreply.github.com>
2024-11-01 11:18:48 -0500

    feat: Added profile bounded context and acquirer entity

---
+++ src/main/java/upc/edu/ecomovil/api/user/application/internal/commandservices/AcquirerCommandServiceImpl.java
@@ -0,0 +1,28 @@
+package upc.edu.ecomovil.api.user.application.internal.commandservices;
+
+import org.springframework.stereotype.Service;
+import upc.edu.ecomovil.api.user.domain.model.commands.CreateAcquirerCommand;
+import upc.edu.ecomovil.api.user.domain.model.entities.Acquirer;
+import upc.edu.ecomovil.api.user.domain.services.AcquirerCommandService;
+import upc.edu.ecomovil.api.user.infrastructure.persistence.jpa.repositories.AcquirerRepository;
+
+import java.util.Optional;
+
+@Service
+public class AcquirerCommandServiceImpl implements AcquirerCommandService {
+    private final AcquirerRepository acquirerRepository;
+
+    public AcquirerCommandServiceImpl(AcquirerRepository acquirerRepository) {
+        this.AcquirerRepository = acquirerRepository;
+    }
+
+    @Override
+    public Optional<Acquirer> handle(CreateAcquirerCommand command){
+        var acquirer = new Acquirer(command);
+        acquirerRepository.save(acquirer);
+        return Optional.of(acquirer);
+
+
+    }
+
+}
