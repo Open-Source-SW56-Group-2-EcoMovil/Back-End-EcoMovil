bfbe237f7b30d64540381423ab3c7f8b7504ff6b
mathiasadriano <129204439+mathiasadriano@users.noreply.github.com>
2024-11-01 12:25:22 -0500

    feat: Added profile bounded context and acquirer entity

---
+++ src/main/java/upc/edu/ecomovil/api/user/interfase/rest/resources/acquirer/AcquirerResource.java
@@ -0,0 +1,4 @@
+package upc.edu.ecomovil.api.user.interfase.rest.resources.acquirer;
+
+public record AcquirerResource(Long id, String name, String lastName, String email) {
+}
---
+++ src/main/java/upc/edu/ecomovil/api/user/interfase/rest/resources/acquirer/CreateAcquirerResource.java
@@ -0,0 +1,4 @@
+package upc.edu.ecomovil.api.user.interfase.rest.resources.acquirer;
+
+public record CreateAcquirerResource(String firstName, String lastName, String email, String phoneNumber) {
+}
