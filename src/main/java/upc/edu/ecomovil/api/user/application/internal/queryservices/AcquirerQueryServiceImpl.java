e6313bac003b85570b39be3805f1cd474786a24b
mathiasadriano <129204439+mathiasadriano@users.noreply.github.com>
2024-11-01 11:20:30 -0500

    feat: Added profile bounded context and acquirer entity

---
+++ src/main/java/upc/edu/ecomovil/api/user/application/internal/queryservices/AcquirerQueryServiceImpl.java
@@ -0,0 +1,25 @@
+package upc.edu.ecomovil.api.user.application.internal.queryservices;
+
+import org.springframework.stereotype.Service;
+import upc.edu.ecomovil.api.user.domain.model.entities.Acquirer;
+import upc.edu.ecomovil.api.user.domain.model.queries.Acquirer.GetAllStudentQuery;
+import upc.edu.ecomovil.api.user.domain.services.AcquirerQueryService;
+import upc.edu.ecomovil.api.user.infrastructure.persistence.jpa.repositories.AcquirerRepository;
+
+import java.util.List;
+
+@Service
+public class AcquirerQueryServiceImpl implements AcquirerQueryService {
+    private final AcquirerRepository acquirerRepository;
+
+    public AcquirerQueryServiceImpl(AcquirerRepository acquirerRepository) {
+        this.acquirerRepository = acquirerRepository;
+    }
+
+
+    @Override
+    public List<Acquirer> handle(GetAllAcquirerQuery query) {
+        return acquirerRepository.findAll();
+    }
+
+}
