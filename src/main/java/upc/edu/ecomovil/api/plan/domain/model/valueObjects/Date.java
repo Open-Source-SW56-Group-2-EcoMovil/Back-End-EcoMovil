c770a5d330c556e9835f4b449b988a373556a305
DavidRm1911 <134657480+DavidRm1911@users.noreply.github.com>
2024-11-13 21:53:54 -0500

    feat: plan2

--- src/main/java/upc/edu/ecomovil/api/plan/domain/model/valueObjects/Date.java
+++
@@ -1,8 +0,0 @@
-package upc.edu.ecomovil.api.plan.domain.model.valueObjects;
-
-import jakarta.persistence.Embeddable;
-
-@Embeddable
-public record Date(int month, int year) {
-    public Date() { this(0, 0); }
-}
---
+++ src/main/java/upc/edu/ecomovil/api/plan2/domain/services/Plan2CommandService.java
@@ -0,0 +1,10 @@
+package upc.edu.ecomovil.api.plan2.domain.services;
+
+import upc.edu.ecomovil.api.plan2.domain.model.aggregates.Plan2;
+import upc.edu.ecomovil.api.plan2.domain.model.commands.CreatePlan2Command;
+
+import java.util.Optional;
+
+public interface Plan2CommandService {
+    Optional<Plan2> handle(CreatePlan2Command command);
+}
